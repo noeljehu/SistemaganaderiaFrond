003552425549a4cec6ebb45fc22236ce337bd322
oscar herrera <oscarherrera@MacBook-Air-de-oscar.local>
2025-03-13 18:34:40 -0500

    Avance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './Componente/login/login.component';
 import { DashboardComponent } from './Componente/Dashboard/dashboard/dashboard.component';
+import { MedicinaFormComponent } from './Componente/Formulario/medicina-form/medicina-form.component';
+import { ProveedorFormComponent } from './Componente/Formulario/proveedor-form/proveedor-form.component';
+import { RegistroAlimentoFormComponent } from './Componente/Formulario/registro-alimento-form/registro-alimento-form.component';
+import { RegistroMedicinaFormComponent } from './Componente/Formulario/registro-medicina-form/registro-medicina-form.component';
+import { UsuarioFormComponent } from './Componente/Formulario/usuario-form/usuario-form.component';
+import { VentaFormComponent } from './Componente/Formulario/venta-form/venta-form.component';
+import { LoginComponent } from './Componente/login/login.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent }, 
    { path: '', redirectTo: '/login', pathMatch: 'full' },
    {path: "dashboard", component:DashboardComponent },
+   {path: "medicina", component:MedicinaFormComponent},
+   {path: "proveedor", component:ProveedorFormComponent},
+   {path: "registro-alimento", component: RegistroAlimentoFormComponent},
+   {path:"registro-medicina", component: RegistroMedicinaFormComponent},
+   {path:"usuario-form", component: UsuarioFormComponent},
+   {path: "venta-form", component:VentaFormComponent}
 ];
